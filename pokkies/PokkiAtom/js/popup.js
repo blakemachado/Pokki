f735ff7d396795ce4f1f29bd8bfd6d96834fc052
Blake Machado <blake.machado@gmail.com>
2011-07-27 13:41:52 -0700

    Improved the readability of popup.js

--- pokkies/PokkiAtom/js/popup.js
+++ pokkies/PokkiAtom/js/popup.js
@@ -1,51 +1,53 @@
+// Global variable for our core app
 var Atom = false;
 
-// Add listener for when the page is unloaded
-// You have 4 seconds from hearing this event to 
-// the pop-up page being disposed
-pokki.addEventListener('popup_unload', onUnload);
-
-function onUnload() {
-    console.log('Pop-up page is being unloaded.');
-	
-	// Time to save any state
-	if(Atom) Atom.onPopupUnload();
-}
-
-// Add listener for when the DOM is loaded
-window.addEventListener('DOMContentLoaded', onLoad, false);
-
-function onLoad() {
-	console.log('Pop-up page is loaded.');
-	
-	// Perform pop-up page initiation and configuration
-	// Time to restore any state
+// Add listener for when the popup is first loaded
+// Perform popup page initiation and configuration
+// NOTE: DOMContentLoaded is the ideal event to listen for as it doesn't
+// wait for external resources (like images) to be loaded
+function load() {
+	console.log('Popup page is loaded.');
 	Atom = new App();
 }
+window.addEventListener('DOMContentLoaded', load, false);
 
-// Add listener for when the pop-up window is showing
-pokki.addEventListener('popup_showing', onShowing);
-
-function onShowing() {
-    console.log('Pop-up window is almost visible.');
-    
-    if(Atom) Atom.onPopupShowing();
+// Add listener for when the page is unloaded by the platform 
+// This occurs due to inactivity or memory usage
+// You have 4 seconds from this event to save/store any data
+function unload() {
+    console.log('Popup page is being unloaded.');
+	// Time to save any state
+	if (Atom) {
+		Atom.onPopupUnload();
+	}
 }
+pokki.addEventListener('popup_unload', unload);
 
-// Add listener for when the pop-up window is shown
-pokki.addEventListener('popup_shown', onShow);
-
-function onShow() {
-    console.log('Pop-up window is visible.');
-    
-    if(Atom) Atom.onPopupShown();
+// Add listener for when the popup window is showing
+function showing() {
+    console.log('Popup window is almost visible.');    
+    if (Atom){
+    	Atom.onPopupShowing();
+    }
 }
+pokki.addEventListener('popup_showing', showing);
+
+
+// Add listener for when the popup window is shown
+function shown() {
+    console.log('Popup window is visible.');
+    if (Atom) {
+    	Atom.onPopupShown();
+    }
+}
+pokki.addEventListener('popup_shown', shown);
+
 
 // Add listener for when the pop-up window is hidden
-pokki.addEventListener('popup_hidden', onHide);
-
-function onHide() {
-    console.log('Pop-up window was closed.');
-    
-    if(Atom) Atom.onPopupHidden();
+function hidden() {
+    console.log('Popup window was hidden.');
+    if (Atom) {
+    	Atom.onPopupHidden();
+    }
 }
+pokki.addEventListener('popup_hidden', hidden);
