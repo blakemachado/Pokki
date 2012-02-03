a2ba1789932f27931c6e769dfeaef9fe9933d7ca
Fontaine Shu <fontaineshu@gmail.com>
2012-02-03 09:19:19 -0800

    added flags to localStorage to only show the enter fullscreen bubble on first install and the exit fullscreen bubble on first fullscreen launch (the reminder is still present)

--- pokkies/PokkiAtomFullscreen/js/app.js
+++ pokkies/PokkiAtomFullscreen/js/app.js
@@ -3,8 +3,8 @@ var App = function() {
         exitfullscreen_bubble,
         exitreminder_bubble,
         upgrade_bubble,
-        shown_fullscreen_bubble = false,
-        shown_exit_bubble = false,
+        shown_fullscreen_bubble = new LocalStore('shown_fullscreen_bubble', {defaultVal: false}), // so the user only sees it on first run
+        shown_exit_bubble = new LocalStore('shown_exit_bubble', {defaultVal: false}), // so the user only sees it on first run
         exitfullscreenTimer = 0,
         controls_left = document.getElementById('controls-left'),
         controls_right = document.getElementById('controls-right'),
@@ -23,14 +23,15 @@ var App = function() {
     
     // initialize tooltip bubbles
     if(this.isPlatformValid(minPlatformVersion)) {
+        // this only gets seen by the user on first install
         enterfullscreen_bubble = Bubbles.createNew('gofullscreen', {
             pinnedTo: '#fullscreen-go', 
             contentHTML: '<strong>Click <span class="ic fsgo"><span></span></span> to play in full screen!</strong>', 
             onHide: function() {
-                shown_fullscreen_bubble = true;
+                shown_fullscreen_bubble.set(true);
             }
         });
-        
+        // this only gets seen by the user the first time they enter fullscreen mode
         exitfullscreen_bubble = Bubbles.createNew('exitfullscreen', {
             pinnedTo: '#fullscreen-exit', 
             contentHTML: '<strong>Click <span class="ic fsexit"><span></span></span> to exit full screen!</strong>',
@@ -39,10 +40,10 @@ var App = function() {
             },
             onHide: function() {
                 exitfullscreenTimer = setTimeout(function() { controls_left.classList.add('_fsexitenabled') }, 200);
-                shown_exit_bubble = true;
+                shown_exit_bubble.set(true);
             }
         });
-        
+        // this is a friendly reminder to the user of how to exit fullscreen
         exitreminder_bubble = Bubbles.createNew('exitfullscreen2', {
             pinnedTo: '#fullscreen-exit', 
             contentHTML: '<strong>Click <span class="ic fsexit"><span></span></span> to exit full screen!</strong>',
@@ -58,10 +59,11 @@ var App = function() {
     }
     else {
         controls_left.style.display = 'none';
+        // optional message to display to users
         upgrade_bubble = Bubbles.createNew('needupgrade', {
             contentHTML: '<strong>Your platform doesn\'t support fullscreen :(</strong><p>Please upgrade to use fullscreen.</p>',
             onHide: function() {
-                shown_fullscreen_bubble = true;
+                shown_fullscreen_bubble.set(true);
             }
         });
     }
@@ -77,9 +79,9 @@ var App = function() {
         var wrapper = document.body;
         wrapper.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
         
-        if(!shown_exit_bubble) {
+        if(!shown_exit_bubble.get()) {
             exitfullscreen_bubble.show();
-            shown_exit_bubble = true;
+            shown_exit_bubble.set(true);
         }
         else {
             exitreminder_bubble.show();
@@ -113,9 +115,9 @@ var App = function() {
         controls_right.style.opacity = 1;
         
         // display bubble tooltip
-        if(!shown_fullscreen_bubble && enterfullscreen_bubble)
+        if(!shown_fullscreen_bubble.get() && enterfullscreen_bubble)
             setTimeout(function() { enterfullscreen_bubble.show(); }, 1200);
-        if(!shown_fullscreen_bubble && upgrade_bubble)
+        if(!shown_fullscreen_bubble.get() && upgrade_bubble)
             setTimeout(function() { upgrade_bubble.show(); }, 1200);
     };
     
