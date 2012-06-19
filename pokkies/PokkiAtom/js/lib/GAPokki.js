54e2707fcf6b0b6dc64ec1c0d1f9f0a312c1661e
Fontaine Shu <fontaineshu@gmail.com>
2012-06-19 13:24:46 -0700

    updated google analytics lib to latest version, removed pokkibrowser.js which is incompatible with the windowed pokki platform

--- pokkies/PokkiAtom/js/lib/GAPokki.js
+++ pokkies/PokkiAtom/js/lib/GAPokki.js
@@ -3,18 +3,18 @@
  * For all details and documentation:
  * https://github.com/blakemachado/Pokki
  *
- * @version     1.3
+ * @version     1.6
  * @license     MIT License
  * @author      Blake Machado <blake@sweetlabs.com>, SweetLabs, Inc.
  *              Fontaine Shu <fontaine@sweetlabs.com>, SweetLabs, Inc.
- * @copyright   (c) 2011, Authors
+ * @copyright   (c) 2011-2012, Authors
  *
  * Example usage:
  * - If you want to track the platform version and pokki version in a custom var, 
  *   include this line above the _setAccount and _setDomain:
  *   ga_pokki._initPlatformCustomVar(--SLOT_NUMBER_DEFAULTS_TO_1--);
  * 
- * - Place these two lines with your values at the bottom of window.html
+ * - Place these two lines with your values at the bottom of popup.html
  *   ga_pokki._setAccount('--GA-ACCOUNT-ID--');
  *   ga_pokki._setDomain('--YOUR-DOMAIN--');
  *
@@ -24,7 +24,7 @@
  */
  
 (function() {
-    var VERSION = '1.3';
+    var VERSION = '1.6';
     var IS_DEBUG = false;
     
     var LocalStorage = function(key, initial_value) {
@@ -55,6 +55,10 @@
         
         var tracking_code_url = 'http://www.google-analytics.com/ga.js';
         var beacon_url = 'http://www.google-analytics.com/__utm.gif';
+        var last_url = '/'; // used to keep track of last page view logged to pass forward to subsequent events tracked
+        var last_nav_url = '/'; // used to keep track of last page actually visited by the user (not popup_hidden or popup_blurred!)
+        var last_page_title = '-'; // used to keep track of last page view logged to pass forward to subsequent events tracked
+        var timer; // used for blur/focus state changes
         
         var utmac = false; // set by calling _setAccount
         var utmhn = false; // set by calling _setDomain
@@ -66,6 +70,21 @@
         var utmt = 'event'; // analytics type
         var utmhid = 0; // unique id per session
         
+        var event_map = {
+			hidden: {
+				path: '/popup_hidden',
+				event: 'PopupHidden'
+			},
+			blurred: {
+				path: '/popup_blurred',
+				event: 'PopupBlurred'
+			},
+			focused: {
+				path: '{last_nav_url}',
+				event: 'PopupFocused'
+			}
+        };
+        
         var uid         = new LocalStorage('ga_pokki_uid');
         var uid_rand    = new LocalStorage('ga_pokki_uid_rand');
         var session_cnt = new LocalStorage('ga_pokki_session_cnt');
@@ -76,6 +95,8 @@
         
         var c_session = 0;
         var custom_vars = visitor_custom_vars._get() ? JSON.parse(visitor_custom_vars._get()) : ['dummy'];
+        
+        var request_cnt = 0;
         
         function rand(min, max) {
             return min + Math.floor(Math.random() * (max - min));
@@ -109,6 +130,14 @@
             }
             return '?' + qa.join('&');
         }
+        
+		function reset_session(c_session) {
+            if(IS_DEBUG) console.log('resetting session');
+            
+            l_session._set(c_session);
+            request_cnt = 0;
+            utmhid = get_random();
+		}
       
         function gainit() {
             c_session = (new Date()).getTime();
@@ -136,22 +165,7 @@
                 l_session._set(c_session);
             }
             
-            // event to reset session when window closes
-            pokki.addEventListener('hidden', function() {
-                // user has specified both required parameters...
-                if(utmac && utmhn) {
-                    that._trackPageview('/hidden');
-                    that._trackEvent('User', 'AppHidden');
-                }
-                
-                if(IS_DEBUG) console.log('resetting session');
-                
-                l_session._set(c_session);
-                request_cnt = 0;
-                utmhid = get_random();
-            });
-            
-            // event to start session when window opens
+            // initialize session when popup opens
             pokki.addEventListener('showing', function() {
                 // don't run the first time
                 if(initialized) {
@@ -165,19 +179,76 @@
                 }
             });
             
-            // event to log an icon click and first run
+            
+			var state = 'hidden';
+			var latestState = '';
+
+			var handleState = function() {
+			
+				if(!utmac || !utmhn) return;
+
+				if(IS_DEBUG) console.log('current state:', state, 'latest state:', latestState);
+
+				switch(state) {
+
+					case 'shown':
+						if (latestState=='hidden') {
+							that._trackPageview(last_nav_url);
+						}
+	                    that._trackEvent('User', 'IconClick');
+	                    // track first run
+	                    if(first_run._get() == null) {
+	                        that._trackEvent('User', 'FirstRun');
+	                        first_run._set(1);
+	                    }
+						break;
+
+					case 'hidden':
+						that._trackPageview(event_map.hidden.path);
+						that._trackEvent('User', event_map.hidden.event);
+						//reset session when popup closes
+						reset_session(c_session);
+						break;
+
+					case 'focused':
+						that._trackPageview(last_nav_url);
+						break;
+
+					case 'blurred':
+						that._trackPageview(event_map.blurred.path);
+						break;
+				
+				}
+
+				latestState = state;
+			
+			};
+
+            pokki.addEventListener('hidden', function() {
+            	state = 'hidden';
+            	clearTimeout(timer);
+            	timer = setTimeout(handleState,200);
+			});
+
             pokki.addEventListener('shown', function() {
-                // user has specified both required parameters...
-                if(utmac && utmhn) {
-                    // track first run
-                    if(first_run._get() == null) {
-                        that._trackEvent('User', 'FirstRun');
-                        first_run._set(1);
-                    }
-                    // track icon click
-                    that._trackEvent('User', 'IconClick');
-                }
-            });
+            	state = 'shown';
+            	clearTimeout(timer);
+            	timer = setTimeout(handleState,200);
+			});
+			
+			// The following window events are only fired IF this ga_pokki library is included in the popup window.
+			// If it's in the background, the window will have to manually make pokki.rpc calls to log the focus and blur events.
+			window.addEventListener('focus',function() {
+				state = 'focused';
+            	clearTimeout(timer);
+            	timer = setTimeout(handleState,200);
+			});
+
+			window.addEventListener('blur',function() {
+				state = 'blurred';
+            	clearTimeout(timer);
+            	timer = setTimeout(handleState,200);
+			});
         }
         
         // public
@@ -209,6 +280,13 @@
             if(IS_DEBUG) console.log(domain);
             utmhn = domain;
         };
+        // public
+        this._setLocale = function(lng,country) {
+            lng = (typeof lng === 'string' && lng.match(/^[a-z][a-z]$/i)) ? lng.toLowerCase() : 'en';
+            country = (typeof country === 'string' && country.match(/^[a-z][a-z]$/i)) ? country.toLowerCase() : 'us';
+            utmul = lng + '-' + country;
+            if(IS_DEBUG) console.log(utmul);
+        };
         
         // public
         this._setCustomVar = function(index, name, value, opt_scope) {
@@ -231,7 +309,6 @@
             
             if(IS_DEBUG) {
                 console.log(custom_vars);
-                //console.trace();
             }
             
             return true;
@@ -241,8 +318,9 @@
         this._trackPageview = function(path, title, source, medium, campaign) {
             if(IS_DEBUG) {
                 console.log('Track Page View', arguments);
-                //console.trace();
             }
+            
+            clearTimeout(timer);
             
             request_cnt++;
             if (!path) {
@@ -275,6 +353,13 @@
                 event += '8(' + names + ')';
                 event += '9(' + values + ')';
                 event += '11(' + scopes + ')';
+            }
+            
+            // remember page path and title for event tracking
+            last_url = path;
+            last_page_title = title;
+            if ([event_map.hidden.path,event_map.blurred.path].indexOf(path) < 0) {
+            	last_nav_url = path;
             }
             
             var params = {
@@ -349,6 +434,8 @@
                 utmt: utmt,
                 utme: event,
                 utmhid: utmhid,
+                utmdt: last_page_title,
+                utmp: last_url,
                 utmac: utmac,
                 utmcc: return_cookies(source, medium, campaign)
             };
@@ -356,6 +443,47 @@
             var img = new Image();
             img.src = url;
         };
+		
+		// public - manually call when ga_pokki is included in the background instead of the window/popup because the
+		// window DOM events on the popup won't be heard by the background
+		this._trackBlur = function() {
+            var e;
+            if(document.createEvent) {
+                e = document.createEvent('HTMLEvents');
+                e.initEvent('blur', true, true);
+            } 
+            else {
+                e = document.createEventObject();
+                e.eventType = 'blur';
+            }
+            
+            if(document.createEvent) {
+                window.dispatchEvent(e);
+            }
+            else {
+                window.fireEvent('on', + e.eventType, e);
+            }
+		};
+		// public - manually call when ga_pokki is included in the background instead of the window/popup because the
+		// window DOM events on the popup won't be heard by the background
+		this._trackFocus = function() {
+            var e;
+            if(document.createEvent) {
+                e = document.createEvent('HTMLEvents');
+                e.initEvent('focus', true, true);
+            } 
+            else {
+                e = document.createEventObject();
+                e.eventType = 'focus';
+            }
+            
+            if(document.createEvent) {
+                window.dispatchEvent(e);
+            }
+            else {
+                window.fireEvent('on', + e.eventType, e);
+            }
+		};
     };
 })();
 
